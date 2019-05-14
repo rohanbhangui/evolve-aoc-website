f264319bd3b61f7dbbdd9fbadfd7a5516eaf0000
Rohan Bhangui <bhanguirohan@gmail.com>
2019-05-14 13:16:00 -0400

    fixed the package json

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,26 +1,147 @@
 import React from 'react';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+
+
 import logo from '../../assets/images/logo.svg';
 import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+firebase.initializeApp({
+  apiKey: "AIzaSyD9Ymp-2H6xWhjdXrVH9AjSJcPMCQvE8ow",
+  authDomain: "evolve-aoc-landing.firebaseapp.com",
+  projectId: "evolve-aoc-landing",
+});
+
+let db = firebase.firestore();
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.addData = this.addData.bind(this);
+  }
+  componentDidMount() {
+  }
+
+  addData(e) {
+    debugger;
+
+    db.collection("products").add({
+      name: 'Hoodie 1',
+      productId: '00001',
+      variants: [
+        {
+          variantId: '00001',
+          image: 'https://via.placeholder.com/350/FF0000/808080',
+          color: {
+            string: 'red',
+            value: '#ff0000'
+          }
+        },
+        {
+          variantId: '00002',
+          image: 'https://via.placeholder.com/350/00FF00/808080',
+          color: {
+            string: 'green',
+            value: '#00ff00'
+          }
+        },
+        {
+          variantId: '00003',
+          image: 'https://via.placeholder.com/350/0000FF/808080',
+          color: {
+            string: 'blue',
+            value: '#0000ff'
+          }
+        }
+      ]
+    });
+
+    db.collection("products").add({
+      name: 'Hoodie 2',
+      productId: '00002',
+      variants: [
+        {
+          variantId: '00001',
+          image: 'https://via.placeholder.com/350/FF0000/808080',
+          color: {
+            string: 'red',
+            value: '#ff0000'
+          }
+        },
+        {
+          variantId: '00002',
+          image: 'https://via.placeholder.com/350/00FF00/808080',
+          color: {
+            string: 'green',
+            value: '#00ff00'
+          }
+        },
+        {
+          variantId: '00003',
+          image: 'https://via.placeholder.com/350/0000FF/808080',
+          color: {
+            string: 'blue',
+            value: '#0000ff'
+          }
+        }
+      ]
+    });
+
+    db.collection("products").add({
+      name: 'Hoodie 3',
+      productId: '00003',
+      variants: [
+        {
+          variantId: '00001',
+          image: 'https://via.placeholder.com/350/FF0000/808080',
+          color: {
+            string: 'red',
+            value: '#ff0000'
+          }
+        },
+        {
+          variantId: '00002',
+          image: 'https://via.placeholder.com/350/00FF00/808080',
+          color: {
+            string: 'green',
+            value: '#00ff00'
+          }
+        },
+        {
+          variantId: '00003',
+          image: 'https://via.placeholder.com/350/0000FF/808080',
+          color: {
+            string: 'blue',
+            value: '#0000ff'
+          }
+        }
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        { /* for adding data <button className="addData" onClick={ this.addData }>add data</button> */ } 
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ webpack.dev.js
@@ -0,0 +1,50 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+const cssPlugin = new MiniCssExtractPlugin({
+    filename: 'styles.css'
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            // translates CSS into CommonJS
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            // compiles Sass to CSS
+            loader: "sass-loader",
+            options: {
+              outputStyle: 'expanded',
+              sourceMap: true,
+              sourceMapContents: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlPlugin, cssPlugin]
+};
