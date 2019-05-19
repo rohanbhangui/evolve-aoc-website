862ed088e11b9271ebdd48f30562476940db3393
Rohan Bhangui <bhanguirohan@gmail.com>
2019-05-19 14:41:38 -0400

    fixed the top bar to be sticky

--- src/components/top-bar/top-bar.js
+++ src/components/top-bar/top-bar.js
@@ -20,27 +20,32 @@ class TopBar extends React.Component {
             <i className="p3 fa fa-shopping-cart fa-stack-1x xfa-inverse" data-count="4b"></i>
           </span>
           <div id="cart-list">
-            <h5 id="cart-title">Cart</h5>
-            { cart.length <= 0 &&
-              <div id="empty-text">Cart is Empty</div>
-            }
+            <div id="cart-header">
+              <h5 id="cart-title">Cart</h5>
+              { cart.length <= 0 &&
+                <div id="empty-text">Cart is Empty</div>
+              }
+            </div>
+            <div id="cart-items">
+              { cart && cart.map((item, i) =>
 
-            { cart && cart.map((item, i) =>
-
-              <div className={`cart-item ${ i > 0 ? 'border-top' : ''}`} key={i} data-id={ item.id } data-variant={ item.variant }>
-                <div className="item-image"> <img src={ item.image } alt={ `${ item.name } - ${ item.color }`} /></div>
-                <div className="item-content">
-                  <div>{ item.name } - { item.color } </div>
+                <div className={`cart-item ${ i > 0 ? 'border-top' : ''}`} key={i} data-id={ item.id } data-variant={ item.variant }>
+                  <div className="item-image"> <img src={ item.image } alt={ `${ item.name } - ${ item.color }`} /></div>
+                  <div className="item-content">
+                    <div>{ item.name } - { item.color } </div>
+                  </div>
+                  <div className="item-price">
+                    <div>{ item.qty } &times; { item.price || "$0.00" }</div>
+                  </div>
                 </div>
-                <div className="item-price">
-                  <div>{ item.qty } &times; { item.price || "$0.00" }</div>
-                </div>
-              </div>
-            )}
+              )}
+            </div>
 
-            { cart.length > 0 &&
-              <button id="checkout-button">Checkout | { "$00.00"}</button>
-            }
+            <div id="cart-footer">
+              { cart.length > 0 &&
+                <button id="checkout-button">Checkout | { "$00.00"}</button>
+              }
+            </div>
           </div>
         </div>
       </div>
