48491e2505761a698d91b43dbaca4e9ca29ebc53
Rohan Bhangui <bhanguirohan@gmail.com>
2019-05-19 12:55:24 -0400

    fixed the checkout button

--- src/components/top-bar/top-bar.js
+++ src/components/top-bar/top-bar.js
@@ -20,22 +20,27 @@ class TopBar extends React.Component {
             <i className="p3 fa fa-shopping-cart fa-stack-1x xfa-inverse" data-count="4b"></i>
           </span>
           <div id="cart-list">
+            <h5 id="cart-title">Cart</h5>
             { cart.length <= 0 &&
-              <div>Cart is Empty</div>
+              <div id="empty-text">Cart is Empty</div>
             }
+
             { cart && cart.map((item, i) =>
 
-              <div className={`cart-item ${ i > 1 ? 'border-top' : ''}`} key={i} data-id={ item.id } data-variant={ item.variant }>
+              <div className={`cart-item ${ i > 0 ? 'border-top' : ''}`} key={i} data-id={ item.id } data-variant={ item.variant }>
                 <div className="item-image"> <img src={ item.image } alt={ `${ item.name } - ${ item.color }`} /></div>
                 <div className="item-content">
                   <div>{ item.name } - { item.color } </div>
-                  <div><strong>Qty:</strong> {item.qty}</div>
                 </div>
                 <div className="item-price">
-                  <div>{ item.price || "$0.00" }</div>
+                  <div>{ item.qty } &times; { item.price || "$0.00" }</div>
                 </div>
               </div>
             )}
+
+            { cart.length > 0 &&
+              <button id="checkout-button">Checkout | { "$00.00"}</button>
+            }
           </div>
         </div>
       </div>
